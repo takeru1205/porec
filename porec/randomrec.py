91c2c644b49987c681a3010472d5a64b22624bf8
takeru1205 <takeru0639418@gmail.com>
2022-11-23 23:05:43 +0900

    [update] adjust to base class

--- porec/randomrec.py
+++ porec/randomrec.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from . import base
 from .dataset import DataSet
 
@@ -7,8 +5,8 @@ from .dataset import DataSet
 class RandomRecommender(base.BaseRecommender):
     """Recommendation algorithm that recommend randomly"""
 
-    def fit(self, data: DataSet) -> None:
+    def fit(self, data: DataSet, *args, **kwargs) -> None:
         self.data: DataSet = data
 
-    def recommend(self, K: int = 1) -> DataSet:
+    def recommend(self, K: int = 1, *args, **kwargs) -> DataSet:
         return self.data.sample(n=K)
