8a82e0136237c6bfa52e4a0114bc818e91e59f80
takeru1205 <takeru0639418@gmail.com>
2022-11-25 23:26:34 +0900

    [update] test evalution values

--- tests/test_randomrecommend.py
+++ tests/test_randomrecommend.py
@@ -23,3 +23,13 @@ class TestRandomRecommender:
         assert len(self.recommender.recommend(3)) == 3
         assert len(self.recommender.recommend(5)) == 5
         assert len(self.recommender.recommend(8)) == 8
+
+    def test_evaluate(self, init_recommender):
+        # the evaluation values are 0 <= v <= 5
+        recs = self.recommender.recommend(5, 0.0, 5.0)
+        assert recs["values"].min() >= 0.0
+        assert recs["values"].max() <= 5.0
+        # the evaluation values are 1 <= v <= 20
+        recs = self.recommender.recommend(5, 1.0, 20.0)
+        assert recs["values"].min() >= 1.0
+        assert recs["values"].max() <= 20.0
