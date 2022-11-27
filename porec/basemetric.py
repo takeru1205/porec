2fe056ba3f397c814c2b488f012897fc3ab2a2ca
takeru1205 <takeru0639418@gmail.com>
2022-11-27 17:12:41 +0900

    [update] add docstrings

--- porec/basemetric.py
+++ porec/basemetric.py
@@ -4,13 +4,20 @@ from dataclasses import dataclass
 
 @dataclass
 class Metric:
-    """Evaluation results"""
+    """Evaluation results
+
+    Args:
+        rmse (float): Root Mean Squared Error
+        precision_k (float): Precision@K
+        recall (float): Recall
+    """
 
     rmse: float = 0.0
     precision_k: float = 0.0
     recall: float = 0.0
 
     def __str__(self):
+        """when calling method, to align the metrics and stdout"""
         return f"""
 Root Mean Squared Error: {self.rmse}
 Precision@K: {self.precision_k}
--- porec/metric.py
+++ porec/metric.py
@@ -16,6 +16,15 @@ class Metrics:
         *args,
         **kwargs,
     ):
+        """Set true values and estimate values
+
+        Args:
+            true_values (List[float]): the ground truth values
+            estimate_values (List[float]): the estimated values
+
+        Returns:
+            None
+        """
         self.true_values = true_values
         self.estimate_values = estimate_values
 
@@ -24,12 +33,23 @@ class Metrics:
         *args,
         **kwargs,
     ) -> Metric:
-        """Calculate RMSE, Precision@K and Recall"""
+        """Calculate RMSE, Precision@K and Recall
+
+        Returns:
+            Metric: returns contain RMSE, Precision@K and Recall.
+            But currently Precision@K and Recall is not available.
+
+        Examples:
+            >>> met = Metrics(true_values, estimate_values)
+            >>> met.evaluate()
+            Metric(rmse=XXX, precision_k=YYY, recall=ZZZ)
+        """
         rmse = self._calc_rmse()
         evals = Metric(rmse=rmse)
         return evals
 
     def _calc_rmse(self):
+        """Calculate root mean squared error true_values and estimate_values"""
         return np.sqrt(
             mean_squared_error(
                 self.true_values,
@@ -38,7 +58,9 @@ class Metrics:
         )
 
     def _calc_precision_k(self):
+        """Calculate precision@K true_values and estimate_values"""
         raise NotImplementedError
 
     def _calc_recall(self):
+        """Calculate recall true_values and estimate_values"""
         raise NotImplementedError
